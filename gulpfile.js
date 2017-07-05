3532851096cea75d291cc192700b3657df6e4445
Vladislav Korbut <v.korbut8@gmail.com>
2017-07-05 13:17:49 +0300

    navbar was completed

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
@@ -9,6 +10,18 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
 });
  
-gulp.task('sass:watch', function () {
+gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+ 
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+gulp.task('default', ['webserver','watch']);
