6d2bf242ae3dbd2108dabb80b9eec88b098c09a9
Vladislav Korbut <v.korbut8@gmail.com>
2017-07-10 14:29:00 +0300

    blog page was completed

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var webserver = require('gulp-webserver');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./css'));
 });
  
