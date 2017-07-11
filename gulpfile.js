8a75c3a6892f4b4b533a548465a9062934ff7f56
Vladislav Korbut <v.korbut8@gmail.com>
2017-07-11 15:16:16 +0300

    file structure was updated, jade was included

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,73 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var webserver = require('gulp-webserver');
 var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var jade = require('gulp-jade');
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+var src = {
+  jade: './src/*.jade',
+  scss:  {
+    styles:'./src/scss/styles.scss',
+    all:'./src/sass/**/*.scss',
+  },
+  fonts: './src/fonts/**/*.*',
+  img:  './src/img/**/*.*',
+}
+
+var dist = {
+  html: './dist/',
+  css:  './dist/css/',
+  fonts: './dist/fonts',
+  img:  './dist/img',
+}
+ 
+gulp.task('jade', function(){
+  gulp.src(src.jade)
+    .pipe(jade())
+    .pipe(gulp.dest(dist.html))
+});
+
+gulp.task('scss', function () {
+  return gulp.src(src.scss.styles)
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./css'));
-});
- 
-gulp.task('watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+    .pipe(cleanCSS({compatibility: 'ie8'}))    
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(dist.css));
 });
 
- 
+gulp.task('img', function() {
+    gulp.src(src.img)
+        .pipe(gulp.dest(dist.img))
+});
+
+gulp.task('fonts', function() {
+    gulp.src(src.fonts)
+        .pipe(gulp.dest(dist.fonts))
+});
+
+gulp.task('scss:watch', function () {
+  gulp.watch(src.scss.all, ['scss']);
+});
+
+gulp.task('jade:watch', function(){
+  gulp.watch(src.jade,['jade']);
+});
+
+gulp.task('img:watch', function () {
+  gulp.watch(src.img, ['img']);
+});
+
+gulp.task('fonts:watch', function(){
+  gulp.watch(src.fonts,['fonts']);
+});
+
+gulp.task('build', ['scss', 'jade', 'img', 'fonts']);
+
+gulp.task('watch', ['scss:watch', 'jade:watch', 'img:watch', 'fonts:watch']);
+
 gulp.task('webserver', function() {
-  gulp.src('./')
+  gulp.src(dist.html)
     .pipe(webserver({
       livereload: true,
       directoryListing: false,
@@ -26,4 +78,4 @@ gulp.task('webserver', function() {
     }));
 });
 
-gulp.task('default', ['sass', 'watch','webserver']);
+gulp.task('default', ['build', 'watch', 'webserver']);
